16295f773ad41eb84d0025b7dc770b989d219f74
nifeng <nifeng@nifengdeMacBook-Air.local>
2026-05-16 13:39:08 +0800

    feat: add address & location fields to enterprise-service job classes

--- enterprise-service/src/main/java/com/parttime/enterprise/pojo/cmd/JobCreateCmd.java
+++ enterprise-service/src/main/java/com/parttime/enterprise/pojo/cmd/JobCreateCmd.java
@@ -2,6 +2,7 @@ package com.parttime.enterprise.pojo.cmd;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,6 +17,18 @@ public class JobCreateCmd {
     private String description;
     @Schema(description = "工作地点")
     private String location;
+    @Schema(description = "省")
+    private String province;
+    @Schema(description = "市")
+    private String city;
+    @Schema(description = "区")
+    private String district;
+    @Schema(description = "详细地址（街道门牌号）")
+    private String address;
+    @Schema(description = "纬度")
+    private BigDecimal latitude;
+    @Schema(description = "经度")
+    private BigDecimal longitude;
     @Schema(description = "岗位分类ID")
     private Long categoryId;
     @Schema(description = "招聘人数")
--- enterprise-service/src/main/java/com/parttime/enterprise/pojo/entity/Job.java
+++ enterprise-service/src/main/java/com/parttime/enterprise/pojo/entity/Job.java
@@ -2,6 +2,7 @@ package com.parttime.enterprise.pojo.entity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
@@ -17,6 +18,18 @@ public class Job {
     private String description;
     @Schema(description = "工作地点")
     private String location;
+    @Schema(description = "省")
+    private String province;
+    @Schema(description = "市")
+    private String city;
+    @Schema(description = "区")
+    private String district;
+    @Schema(description = "详细地址（街道门牌号）")
+    private String address;
+    @Schema(description = "纬度")
+    private BigDecimal latitude;
+    @Schema(description = "经度")
+    private BigDecimal longitude;
     @Schema(description = "岗位分类ID")
     private Long categoryId;
     @Schema(description = "招聘人数")
