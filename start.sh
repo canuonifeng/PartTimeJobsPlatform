2aaefef819aa219ecb9b011375f44d248d0c8aab
nifeng <nifeng@nifengdeMacBook-Air.local>
2026-06-06 20:11:25 +0800

    fix

--- start.sh
+++ start.sh
@@ -15,3 +15,19 @@ mvn clean package -Dmaven.test.skip=true
 cd target
 nohup java -jar platform-service-1.0.0-SNAPSHOT.jar > ../../logs/platform-service.log 2>&1 &
 cd ../../
+
+cd enterprise-pc
+npm run build
+cd ../
+
+cd enterprise-uniapp
+npm run build
+cd ../
+
+cd platform-pc
+npm run build
+cd ../
+
+cd worker-uniapp
+npm run build
+cd ../
