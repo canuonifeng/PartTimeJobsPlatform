25436c5037301b7ca66ee45a26049290d6433153
nifeng <nifeng@nifengdeMacBook-Air.local>
2026-06-08 21:58:11 +0800

    feat: add WeChat Pay configuration class

---
+++ c-service/src/main/java/com/parttime/cservice/config/WeChatPayConfig.java
@@ -0,0 +1,18 @@
+package com.parttime.cservice.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "wechat.pay")
+public class WeChatPayConfig {
+
+    private String mchId;
+    private String apiKey;
+    private String certPath;
+    private String privateKeyPath;
+    private String notifyUrl;
+    private String apiV3Key;
+}
