9a5cc0bfe99f55184197c8dfc83db93ca5e18d72
nifeng <nifeng@nifengdeMacBook-Air.local>
2026-06-12 13:21:32 +0800

    chore: 调整线上启动配置

--- start.sh
+++ start.sh
@@ -5,19 +5,19 @@ lsof -ti:8083 | xargs kill -9 2>/dev/null;
 cd c-service 
 mvn clean package -Dmaven.test.skip=true
 cd target
-nohup java -jar c-service-1.0.0-SNAPSHOT.jar > ../../logs/c-service.log 2>&1 &
+nohup java -jar c-service-1.0.0-SNAPSHOT.jar --spring.profiles.active=prod --spring.config.additional-location=optional:file:../../config/c-service-env.yml > ../../logs/c-service.log 2>&1 &
 cd ../../
 
 cd enterprise-service 
 mvn clean package -Dmaven.test.skip=true
 cd target
-nohup java -jar enterprise-service-1.0.0-SNAPSHOT.jar > ../../logs/enterprise-service.log 2>&1 &
+nohup java -jar enterprise-service-1.0.0-SNAPSHOT.jar --spring.profiles.active=prod --spring.config.additional-location=optional:file:../../config/enterprise-service-env.yml > ../../logs/enterprise-service.log 2>&1 &
 cd ../../
 
 cd platform-service 
 mvn clean package -Dmaven.test.skip=true
 cd target
-nohup java -jar platform-service-1.0.0-SNAPSHOT.jar > ../../logs/platform-service.log 2>&1 &
+nohup java -jar platform-service-1.0.0-SNAPSHOT.jar --spring.profiles.active=prod --spring.config.additional-location=optional:file:../../config/platform-service-env.yml > ../../logs/platform-service.log 2>&1 &
 cd ../../
 
 cd enterprise-pc
