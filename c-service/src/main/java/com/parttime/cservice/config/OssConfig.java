b48091edd1ed2336b4ec131682dcda40d2f0c505
nifeng <nifeng@nifengdeMacBook-Air.local>
2026-09-24 14:21:21 +0800

    fix: require oss access-key-secret in client conditional

--- c-service/src/main/java/com/parttime/cservice/config/OssConfig.java
+++ c-service/src/main/java/com/parttime/cservice/config/OssConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnExpression(
         "T(org.springframework.util.StringUtils).hasText('${oss.endpoint:}')"
                 + " && T(org.springframework.util.StringUtils).hasText('${oss.access-key-id:}')"
+                + " && T(org.springframework.util.StringUtils).hasText('${oss.access-key-secret:}')"
 )
 public class OssConfig {
 
